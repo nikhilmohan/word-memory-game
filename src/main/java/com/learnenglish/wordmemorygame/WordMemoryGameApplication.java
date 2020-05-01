4e76f693ae9c7f608eb33ebb12b0ee9c6633a527
Nikhil Mohan <nikhil_mohan@infosys.com>
2020-05-01 10:05:11 +0530

    initial commit

---
+++ src/main/java/com/learnenglish/wordmemorygame/WordMemoryGameApplication.java
@@ -0,0 +1,13 @@
+package com.learnenglish.wordmemorygame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordMemoryGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordMemoryGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnenglish/wordmemorygame/WordMemoryGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnenglish.wordmemorygame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordMemoryGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
